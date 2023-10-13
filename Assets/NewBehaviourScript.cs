fc8220ccaeefe41ad56308709ad378828266862a
VeraPerez <111065613+VeraPerez@users.noreply.github.com>
2023-10-13 15:38:51 -0500

    dos escenas con interacciones

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //otras funciones que realizan nuevas acciones
+    void interacciones()
+    {
+        //Obtiene el número de la escena donde se obtiene el código
+        int numeroescena = gameObject.scene.rootCount;
+        SceneManager.LoadScene(numeroescena + 1);
+    }
+        private void OnCollisionEnter(Collision collision)
+    {
+            interacciones();
+
+    }
+}
+
